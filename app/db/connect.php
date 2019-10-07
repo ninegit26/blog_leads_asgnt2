d3d60aa8cf6b815233da951bcc427d30ad01fcc7
Rogerio Peyroton <rogeriobp@gmail.com>
2019-10-07 12:56:33 -0300

    conexão db

---
+++ app/db/connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = 'beabla-blog.mysql.uhserver.com';
+$user = 'beaba_admin';
+$pass = 'beaba132@@';
+$db = 'beabla_blog';
+
+$connection = new MySQLi($host, $user, $pass, $db_name);
+
+if ($connection->connect_error) {
+    die('Erro na conexão com o DB: ' . $connection->connect_error);
+}
